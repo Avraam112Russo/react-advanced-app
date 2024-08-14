b1e8e1c5527d562acb2b0fee86587a521f9589d6
Russo Avram <avraam112russo@gmail.com>
2024-08-14 23:11:09 +0300

    commit for test pull request

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -24,6 +24,7 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
 
     ]
     // we need this plugins only in development
+    // ci pipeline work without this plugins
     if (isDev == true){
     plugins.push( new webpack.HotModuleReplacementPlugin()) // accept code changes without refresh page,);
     plugins.push(new BundleAnalyzerPlugin({openAnalyzer: false}))// auto open user interface for detail analyze http://127.0.0.1:8888/
