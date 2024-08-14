322637c876184d10f327849f3917de4a0d3dbf18
Russo Avram <avraam112russo@gmail.com>
2024-08-14 23:01:26 +0300

    added ci pipline, remove development plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -7,23 +7,26 @@ import {BundleAnalyzerPlugin} from "webpack-bundle-analyzer";
 
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[]{
 
-    return [
-        new HtmlWebpackPlugin({ // generate index.html in build output
-            template: paths.html
-        }),
-        new webpack.ProgressPlugin(), // just show status build project (10%, 20%, 30% etc.)
+    const plugins= [
+            new HtmlWebpackPlugin({ // generate index.html in build output
+                template: paths.html
+            }),
+            new webpack.ProgressPlugin(), // just show status build project (10%, 20%, 30% etc.)
 
-        // separate multiple css files
-        new MiniCssExtractPlugin({
-            filename: "css/[name].[contenthash:8].css",
-            chunkFilename: "css/[name].[contenthash:8].css"
-        }),
-        new webpack.DefinePlugin({
-            __IS_DEV__ : isDev //  use variable __IS_DEV in i18n
-        }),
-        new webpack.HotModuleReplacementPlugin(), // accept code changes without refresh page,
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false // auto open user interface
-        }) // plugin for bundle details analyze http://127.0.0.1:8888/ user interface
+            // separate multiple css files
+            new MiniCssExtractPlugin({
+                filename: "css/[name].[contenthash:8].css",
+                chunkFilename: "css/[name].[contenthash:8].css"
+            }),
+            new webpack.DefinePlugin({
+                __IS_DEV__ : isDev //  use variable __IS_DEV in i18n
+            })
+
     ]
+    // we need this plugins only in development
+    if (isDev == true){
+    plugins.push( new webpack.HotModuleReplacementPlugin()) // accept code changes without refresh page,);
+    plugins.push(new BundleAnalyzerPlugin({openAnalyzer: false}))// auto open user interface for detail analyze http://127.0.0.1:8888/
+    }
+    return plugins;
 }
