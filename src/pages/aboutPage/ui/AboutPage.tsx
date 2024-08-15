a38338c392f307b720a16569519b1c6d42cd65a7
Russo Avram <avraam112russo@gmail.com>
2024-08-15 20:46:11 +0300

    commit

--- src/pages/aboutPage/ui/AboutPage.tsx
+++ src/pages/aboutPage/ui/AboutPage.tsx
@@ -4,6 +4,7 @@ import {useTranslation} from "react-i18next";
 const AboutPage = () => {
     const {t} = useTranslation('aboutPage'); // loading translate only aboutPage.json chunk
     return (
+        //
         <div>
             {t('О сайте')}
         </div>
